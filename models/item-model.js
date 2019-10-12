59449130cfca3bd4769fdcae98a73b162cb2aaa3
Peter Bognár <bognarpaeter@gmail.com>
2019-10-12 18:58:44 +0200

    added item model

--- models/item-model.js
+++ models/item-model.js
@@ -1,27 +1,50 @@
 'use strict';
 
 var mongoose = require('mongoose');
-
 var ItemSchema = mongoose.Schema({
-    user_id: {
+    id: {
     	type: String
     },
     name: {
         type: String
+    },
+    type: {
+        type: String
+    },
+    blockedBy: {
+        type: String
+    },
+    offeredBy: {
+        type: String
+    },
+    amount: {
+        type: Number
+    },
+    unit: {
+        type: String
+    },
+    expiration_date: {
+        type: Date
+    },
+    image: {
+        type: String
+    },
+    location: {
+        lon: { type: Number },
+        lat: { type: Number }
     }
 });
 
 const Item = module.exports = mongoose.model('Item', ItemSchema);
 
-var createItem = function(newItem, callback){
-    newItem.name = "valami";
-    newItem.save(callback);
+var getItemsByBlocker = function(username, callback){
+    Item.find({blockedBy: username}, callback);
 };
 
-var getItemsByRequestId = function(request_id, callback){
-    // want to have multiple not just one
-    Item.find({user_id}, callback);
+var getItemsByOffer = function(username, callback){
+    Item.find({offeredBy: username}, callback);
 };
 
 module.exports.createItem = createItem;
-module.exports.getItemsByRequestId = getItemsByRequestId;
+module.exports.getItemsByBlocker = getItemsByBlocker;
+module.exports.getItemsByOffer = getItemsByOffer;
