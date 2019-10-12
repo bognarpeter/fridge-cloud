1af4abe4715cc75baf0916dbeb9bda5d722fb436
Gergely Tusko <gergely.tusko@gmail.com>
2019-10-12 15:07:47 +0200

    Add view for user's published items

--- index.js
+++ index.js
@@ -106,7 +106,6 @@ function initialize () {
     });
 
     app.get('/my-items', (req, res) => {
-        var person = "Gaudi";
         res.render('my-items', {food: [
           {
             "id": 0,
@@ -125,6 +124,28 @@ function initialize () {
           }
         ]});
     });
+
+
+      app.get('/my-published-items', (req, res) => {
+          res.render('my-published-items', {food: [
+            {
+              "id": 0,
+              "name": "apple",
+              "type": "vegetable",
+              "blockedBy": "Peter",
+              "offeredBy": "Simon",
+              "amount": 0,
+              "unit": "Pieces",
+              "expiration_date": "2019-10-12T03:15:01.588Z",
+              "image": "https://google.de/image.jpg",
+              "location": {
+                "lon": 0,
+                "lat": 0
+              }
+            }
+          ]});
+      });
+
     //Frontend Routes
     require('./routes/frontend')(app);
 
